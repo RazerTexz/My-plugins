53dcb4e18f8a10cd955a56bb17e3f6b8f0c40f33
RazerTexz <66409416+RazerTexz@users.noreply.github.com>
2025-11-26 22:06:48 +0700

    SilentMessages: v1.0.1

--- plugins/SilentMessages/build.gradle.kts
+++ plugins/SilentMessages/build.gradle.kts
@@ -1,8 +1,11 @@
-version = "1.0.0"
+version = "1.0.1"
 description = "Type '@silent' at the start of your message"
 
 aliucord {
     changelog = """
+        # 1.0.1
+        * Added a indicator for silent messages (the icon is temporary)
+
         # 1.0.0
         * Initial version
     """.trimIndent()
--- plugins/SilentMessages/src/main/kotlin/com/github/razertexz/Main.kt
+++ plugins/SilentMessages/src/main/kotlin/com/github/razertexz/Main.kt
@@ -1,6 +1,10 @@
 package com.github.razertexz
 
+import androidx.constraintlayout.widget.ConstraintLayout
 import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
@@ -8,15 +12,18 @@ import com.aliucord.patcher.*
 import com.aliucord.Utils
 import com.aliucord.Http
 
-import com.discord.api.message.allowedmentions.MessageAllowedMentions
-import com.discord.api.message.attachment.MessageAttachment
-import com.discord.api.message.MessageReference
-import com.discord.api.message.Message
-
 import com.discord.stores.StoreStream
 import com.discord.stores.StoreNotifications
 import com.discord.utilities.rest.SendUtils
 import com.discord.utilities.messagesend.`MessageQueue$doSend$2`
+import com.discord.widgets.chat.list.adapter.WidgetChatListAdapterItemMessage
+import com.discord.api.message.allowedmentions.MessageAllowedMentions
+import com.discord.api.message.attachment.MessageAttachment
+import com.discord.api.message.MessageReference
+import com.discord.api.message.Message as ApiMessage
+import com.discord.models.message.Message as ModelMessage
+
+import com.lytefast.flexinput.R
 
 @AliucordPlugin(requiresRestart = false)
 class Main : Plugin() {
@@ -30,6 +37,10 @@ class Main : Plugin() {
     )
 
     override fun start(ctx: Context) {
+        val viewId = View.generateViewId()
+        val icon = ctx.getDrawable(R.e.ic_sound_quiet_24dp)!!.mutate()
+        Utils.tintToTheme(icon)
+
         patcher.before<`MessageQueue$doSend$2`<*, *>>("call", SendUtils.SendPayload.ReadyToSend::class.java) {
             val message = `$message`
             if (message.content.startsWith("@silent ")) {
@@ -41,12 +52,48 @@ class Main : Plugin() {
             }
         }
 
-        patcher.before<StoreNotifications>("handleMessageCreate", Message::class.java) {
-            val message = it.args[0] as Message
-            if (message.g() != StoreStream.getChannelsSelected().getId() && (message.l() and 4096L) != 0L) {
+        patcher.before<StoreNotifications>("handleMessageCreate", ApiMessage::class.java) {
+            val message = it.args[0] as ApiMessage
+            if (message.g() != StoreStream.getChannelsSelected().getId() && (message.l() and 4096L) != 0L)
                 it.result = null
+        }
+
+        patcher.after<WidgetChatListAdapterItemMessage>("configureThreadSpine", ModelMessage::class.java, Boolean::class.java) {
+            val message = it.args[0] as ModelMessage
+            if (!message.isLoading) {
+                val layout = (itemView as ViewGroup).getChildById<ConstraintLayout>(Utils.getResId("chat_list_adapter_item_text_header", "id"))!!
+                val indicator = layout.getChildById<ImageView>(viewId) ?: ImageView(itemView.context).apply {
+                    id = viewId
+                    scaleX = 0.75f
+                    scaleY = 0.75f
+                    setImageDrawable(icon)
+
+                    layout.addView(this, ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT).apply {
+                        val id = Utils.getResId("chat_list_adapter_item_text_timestamp", "id")
+
+                        startToEnd = id
+                        topToTop = id
+                        bottomToBottom = id
+                    })
+                }
+
+                indicator.visibility = if ((message.flags and 4096L) != 0L) View.VISIBLE else View.GONE
             }
         }
+    }
+
+    private inline fun <reified T : View> ViewGroup.getChildById(id: Int): T? {
+        var idx = childCount - 1
+
+        while (idx >= 0) {
+            val child = getChildAt(idx)
+            if (child is T && child.id == id)
+                return child as T
+
+            idx--
+        }
+
+        return null
     }
 
     override fun stop(ctx: Context) = patcher.unpatchAll()
