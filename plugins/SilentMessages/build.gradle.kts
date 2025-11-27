e02ad6de76b124cc7e0f61edcd5f2642b7dd4e33
RazerTexz <66409416+RazerTexz@users.noreply.github.com>
2025-11-27 19:31:59 +0700

    SilentMessages: v1.0.5

--- plugins/SilentMessages/build.gradle.kts
+++ plugins/SilentMessages/build.gradle.kts
@@ -1,4 +1,4 @@
-version = "1.0.4"
+version = "1.0.5"
 description = "Type '@silent' at the start of your message"
 
 aliucord {
--- plugins/SilentMessages/src/main/kotlin/com/github/razertexz/Main.kt
+++ plugins/SilentMessages/src/main/kotlin/com/github/razertexz/Main.kt
@@ -11,31 +11,22 @@ import com.aliucord.entities.Plugin
 import com.aliucord.patcher.*
 import com.aliucord.Utils
 import com.aliucord.Http
+import com.aliucord.utils.GsonUtils
 
 import com.discord.stores.StoreStream
 import com.discord.stores.StoreNotifications
 import com.discord.utilities.rest.SendUtils
 import com.discord.utilities.messagesend.`MessageQueue$doSend$2`
 import com.discord.widgets.chat.list.adapter.WidgetChatListAdapterItemMessage
-import com.discord.api.message.allowedmentions.MessageAllowedMentions
-import com.discord.api.message.attachment.MessageAttachment
-import com.discord.api.message.MessageReference
 import com.discord.api.message.Message as ApiMessage
 import com.discord.models.message.Message as ModelMessage
 
 import com.lytefast.flexinput.R
 
+import rx.Observable
+
 @AliucordPlugin(requiresRestart = false)
 class Main : Plugin() {
-    private class MessagePayload(
-        val content: String,
-        val nonce: String,
-        val allowed_mentions: MessageAllowedMentions?,
-        val attachments: List<MessageAttachment>,
-        val message_reference: MessageReference?,
-        val flags: Int
-    )
-
     override fun start(ctx: Context) {
         val viewId = View.generateViewId()
         val icon = ctx.getDrawable(R.e.ic_notifications_off_grey_24dp)!!.mutate()
@@ -44,11 +35,16 @@ class Main : Plugin() {
         patcher.before<`MessageQueue$doSend$2`<*, *>>("call", SendUtils.SendPayload.ReadyToSend::class.java) {
             val message = `$message`
             if (message.content.startsWith("@silent ")) {
-                Utils.threadPool.execute {
-                    Http.Request.newDiscordRNRequest("https://discord.com/api/v10/channels/${message.channelId}/messages", "POST").executeWithJson(MessagePayload(message.content.substring(7).trimStart(), message.nonce, message.allowedMentions, message.attachments, message.messageReference, 4096))
+                it.result = Observable.D { // fromCallable
+                    Http.Request.newDiscordRNRequest("https://discord.com/api/v10/channels/${message.channelId}/messages", "POST").executeWithJson(mapOf(
+                        "content" to message.content.substring(7).trimStart(),
+                        "nonce" to message.nonce,
+                        "allowed_mentions" to message.allowedMentions,
+                        "attachments" to message.attachments,
+                        "message_reference" to message.messageReference,
+                        "flags" to 4096
+                    )).json(GsonUtils.gsonRestApi, ApiMessage::class.java)
                 }
-
-                it.result = null
             }
         }
 
